4f75a541e2cf98fd8fe4b0f4b95e2249010b2756
Camila Zagal <camilazags@gmail.com>
2022-10-04 22:38:11 -0500

    fixed selectWord function

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ const playBtn = document.getElementsByClassName('play-btn');
 
 const words = ['javascript', 'html', 'variable', 'loop', 'github', 'vscode', 'coding', 'function'];
 
+let playingWord = "";
+
+//Function for selecting word from array
 function selectWord () {
    const index = Math.floor(Math.random() * words.length);
-   const playingWord = words[index];
-   console.log(playingWord);
+   const selectedWord = words[index];
+   playingWord = selectedWord;
+   console.log(`Your selected word is: ${playingWord}.`);
 }
+
 
