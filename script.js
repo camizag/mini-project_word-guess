97afad5489c73a14bf0b9a04897224451411ddeb
Camila Zagal <camilazags@gmail.com>
2022-10-05 13:55:28 -0500

    added function for transforming the word

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const wins = document.getElementsByClassName('wins');
 const losses = document.getElementsByClassName('losses');
 const playBtn = document.getElementsByClassName('play-btn');
 
+//Array with words available for play
 const words = ['javascript', 'html', 'variable', 'loop', 'github', 'vscode', 'coding', 'function'];
 
+//Global Variables
 let playingWord = "";
+let transformedWord = [];
 
 //Function for selecting word from array
 function selectWord () {
@@ -13,6 +16,16 @@ function selectWord () {
    const selectedWord = words[index];
    playingWord = selectedWord;
    console.log(`Your selected word is: ${playingWord}.`);
+   transformWord();
 }
 
-
+//Function to replace the letters of the word with '_'
+function transformWord() {
+   let wLength = playingWord.length;
+   transformedWord.length = 0;
+   while (wLength > 0) {
+      let count = transformedWord.push(" _ ");
+      wLength--;
+   } 
+   console.log(`Your transformed word is: ${transformedWord}.`);
+}
